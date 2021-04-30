7070053b08c416e71b2ac735b5850e57b1a8eb23
Daniel Zou <zoudl2000@gmail.com>
2021-04-30 02:57:46 -0500

    Annealing seems to work now

--- solver.py
+++ solver.py
@@ -5,7 +5,7 @@ from parse import read_input_file, write_output_file
 from utils import is_valid_solution, calculate_score
 from os.path import basename, normpath, exists, dirname
 from os import makedirs
-from math import exp
+from math import exp, log
 import random
 import sys
 import glob
@@ -60,58 +60,58 @@ def solve_greedy(G):
     return c, k
 
 
-def solve_greedy_2(G):
-    H = G.copy()
-    num_nodes = H.number_of_nodes()
-    s, t = 0, num_nodes - 1
-    c_max, k_max = get_constraints(num_nodes)
-    c, k = [], []
-    T = int(0.5 * num_nodes ** 2)
+# def solve_greedy_2(G):
+#     H = G.copy()
+#     num_nodes = H.number_of_nodes()
+#     s, t = 0, num_nodes - 1
+#     c_max, k_max = get_constraints(num_nodes)
+#     c, k = [], []
+#     T = int(0.5 * num_nodes ** 2)
 
-    for i in range(1, T):
-        # Probability of backtracking by removing random node from c or edge from k
-        back_prob = exp(-i / num_nodes)
-        if back_prob > random.random() and len(c) + len(k) > 0:
-            a = random.choice(c + k)
-            if isinstance(a, int):
-                c.remove(a)
-                H.add_node(a)
-                for v in range(t):
-                    if G.has_edge(a, v):
-                        H.add_edge(a, v, **G.get_edge_data(a, v))
-            else:
-                k.remove(a)
-                H.add_edge(*a, **G.get_edge_data(*a))
-            continue
+#     for i in range(1, T):
+#         # Probability of backtracking by removing random node from c or edge from k
+#         back_prob = exp(-i / num_nodes)
+#         if back_prob > random.random() and len(c) + len(k) > 0:
+#             a = random.choice(c + k)
+#             if isinstance(a, int):
+#                 c.remove(a)
+#                 H.add_node(a)
+#                 for v in range(t):
+#                     if G.has_edge(a, v):
+#                         H.add_edge(a, v, **G.get_edge_data(a, v))
+#             else:
+#                 k.remove(a)
+#                 H.add_edge(*a, **G.get_edge_data(*a))
+#             continue
 
-        # Run greedy algorithm
-        SP_nodes, SP_edges, SP = get_SP(H, s, t)
-        rm_node = (None, SP)
-        for node in SP_nodes:
-            H_temp = H.copy()
-            H_temp.remove_node(node)
-            if is_connected(H_temp):
-                _, _, SP_temp = get_SP(H_temp, s, t)
-                if SP_temp >= rm_node[1]:
-                    rm_node = (node, SP_temp)
-        rm_edge = (None, SP)
-        for edge in SP_edges:
-            H_temp = H.copy()
-            H_temp.remove_edge(*edge)
-            if is_connected(H_temp):
-                _, _, SP_temp = get_SP(H_temp, s, t)
-                if SP_temp >= rm_edge[1]:
-                    rm_edge = (edge, SP_temp)
+#         # Run greedy algorithm
+#         SP_nodes, SP_edges, SP = get_SP(H, s, t)
+#         rm_node = (None, SP)
+#         for node in SP_nodes:
+#             H_temp = H.copy()
+#             H_temp.remove_node(node)
+#             if is_connected(H_temp):
+#                 _, _, SP_temp = get_SP(H_temp, s, t)
+#                 if SP_temp >= rm_node[1]:
+#                     rm_node = (node, SP_temp)
+#         rm_edge = (None, SP)
+#         for edge in SP_edges:
+#             H_temp = H.copy()
+#             H_temp.remove_edge(*edge)
+#             if is_connected(H_temp):
+#                 _, _, SP_temp = get_SP(H_temp, s, t)
+#                 if SP_temp >= rm_edge[1]:
+#                     rm_edge = (edge, SP_temp)
 
-        if rm_node[0] and len(c) < c_max and rm_node[1] >= rm_edge[1]:
-            H.remove_node(rm_node[0])
-            c.append(rm_node[0])
-        elif rm_edge[0] and len(k) < k_max:
-            H.remove_edge(*rm_edge[0])
-            k.append(rm_edge[0])
-        else:
-            continue
-    return c, k
+#         if rm_node[0] and len(c) < c_max and rm_node[1] >= rm_edge[1]:
+#             H.remove_node(rm_node[0])
+#             c.append(rm_node[0])
+#         elif rm_edge[0] and len(k) < k_max:
+#             H.remove_edge(*rm_edge[0])
+#             k.append(rm_edge[0])
+#         else:
+#             continue
+#     return c, k
 
 
 def solve_anneal(G):
@@ -119,43 +119,50 @@ def solve_anneal(G):
     num_nodes = H.number_of_nodes()
     s, t = 0, num_nodes - 1
     c_max, k_max = get_constraints(num_nodes)
-    c, k = [], []
-    i, i_max = 1, num_nodes ** 2
+    c, k = set(), set()
+    best = {"c": c, "k": k, "SP": float("-inf")}
+    i, i_max = 1, int(log(num_nodes)) * num_nodes ** 2
     T0 = i_max
 
     while i < i_max:
+        if i % 100 == 0:
+            GSP = dijkstra_path_length(G, s, t)
+            SP = dijkstra_path_length(H, s, t)
+            print(SP - GSP)
+
         # Create search space for neighbors
         SP_nodes, SP_edges, SP = get_SP(H, s, t)
-        neighbors = {}
-        for a in c + k:
-            neighbors[a] = 0 # Remove from c or k
+        move_space = {}
+        for a in c | k:
+            move_space[a] = 0 # Remove from c or k
         if len(c) < c_max:
             for a in SP_nodes:
-                neighbors[a] = 1 # Add to c
+                move_space[a] = 1 # Add to c
         if len(k) < k_max:
             for a in SP_edges:
-                neighbors[a] = 1 # Add to k
+                move_space[a] = 1 # Add to k
 
         # Pick a neighbor
-        obj, add_to = random.choice(list(neighbors.items()))
+        a, add_to = random.choice(list(move_space.items()))
         H_temp = H.copy()
         if add_to:
-            if isinstance(obj, int): # Check if obj is node
-                H_temp.remove_node(obj)
+            if isinstance(a, int): # Check if a is node
+                H_temp.remove_node(a)
             else:
-                H_temp.remove_edge(*obj)
+                H_temp.remove_edge(*a)
         else:
-            if isinstance(obj, int):
-                H_temp.add_node(obj)
-                for v in range(t):
-                    if G.has_edge(obj, v):
-                        H_temp.add_edge(obj, v, **G.get_edge_data(obj, v))
+            if isinstance(a, int):
+                H_temp.add_node(a)
+                for v in G.neighbors(a):
+                    if (a, v) not in k and (v, a) not in k:
+                        H_temp.add_edge(a, v, **G.get_edge_data(a, v))
             else:
-                H_temp.add_edge(*obj, **G.get_edge_data(*obj))
+                H_temp.add_edge(*a, **G.get_edge_data(*a))
         if not is_connected(H_temp):
+            i += 1
             continue
 
-        # Move to neighbor with probability
+        # Make move with probability
         _, _, SP_move = get_SP(H_temp, s, t)
         if SP_move > SP:
             prob = 1
@@ -164,27 +171,25 @@ def solve_anneal(G):
             prob = exp((SP_move - SP) / T)
         if prob > random.random():
             if add_to:
-                if isinstance(obj, int):
-                    c.append(obj)
+                if isinstance(a, int):
+                    c.add(a)
                 else:
-                    k.append(obj)
+                    k.add(a)
             else:
-                if isinstance(obj, int):
-                    c.remove(obj)
+                if isinstance(a, int):
+                    c.remove(a)
                 else:
-                    k.remove(obj)
+                    k.remove(a)
             H = H_temp
+
+        if SP_move > best["SP"]:
+            best["c"] = c.copy()
+            best["k"] = k.copy()
+            best["SP"] = SP_move
         i += 1
 
-    GSP = dijkstra_path_length(G, s, t)
-    SP = dijkstra_path_length(H, s, t)
-    print(SP - GSP)
+    return best["c"], best["k"]
 
-    return c, k
-
-
-def solve_genetic(G):
-    pass
 
 def get_constraints(nodes):
     "Return constraints for c, k"
